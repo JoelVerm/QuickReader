f4ba31db1db16da0c11546728d2bc626a1f0d89c
Joël Vermeulen <98051242+JoelVerm@users.noreply.github.com>
2023-02-03 09:37:35 +0100

    main layout start

--- app/src/main/java/com/j4a/quickreader/MainActivity.kt
+++ app/src/main/java/com/j4a/quickreader/MainActivity.kt
@@ -2,10 +2,35 @@ package com.j4a.quickreader
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import com.google.common.util.concurrent.ListenableFuture
+import java.util.concurrent.CompletableFuture
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var cameraProviderFuture:ListenableFuture<ProcessCameraProvider>
+    private lateinit var cameraSelector:CameraSelector
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val shootButton = findViewById<Button>(R.id.shootButton)
+        shootButton.setOnClickListener {
+            it.background.setTint(5)
+        }
+
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+    }
+
+    fun startCamera() {
+//        cameraProviderFuture.addListener({
+//            val cameraProvider = cameraProviderFuture.get()
+//
+//            val preview = Preview.Builder().build()
+//        })
     }
 }
