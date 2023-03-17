4951b051521bda9ecc8f01605ba355f4bc281c7d
Joël Vermeulen <98051242+JoelVerm@users.noreply.github.com>
2023-03-17 08:06:18 +0100

    quick fix

--- app/src/main/java/com/j4a/quickreader/ImageBitReader.kt
+++ app/src/main/java/com/j4a/quickreader/ImageBitReader.kt
@@ -1,74 +1,63 @@
 package com.j4a.quickreader
 
 import android.graphics.BitmapFactory
+import android.graphics.Color
 import androidx.camera.core.ImageProxy
 
 class ImageBitReader(private val image: ImageProxy) {
     fun read() {
-        val buffer = image.planes[0].buffer
-        val bytes = byteArrayOf()
-        buffer.get(bytes)
-        val bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
-        for (p in image.planes) {
-            print(p.buffer)
-        }
+        val pixelArray = readColors()
     }
 
-    fun jorian(){
-        //Color determination
-//QR code is 21x21
-import android.media.Image
-import android.media.Image.Plane
-import android.graphics.Color
+    fun readColors(): Array<Array<Int>> {
+        val imageWidth = image.width
+        val imageHeight = image.height
+        val imageArray = arrayOf<Array<Int>>()
 
-val seventhRow = arrayOf(1,1,1,1,1,1,1,0,1,0,1,0,1,0,1,1,1,1,1,1,1) //moet 2d array zijn
-val image = Image("path/to/qr/code")
-val colorValue
-var imageWidth = image.getWidth()
-var imageHeight = image.getHeight()
-val imageArray = arrayOf<Array<Int>>()
+        val buffer = image.planes[0].buffer
+        val bytes = ByteArray(buffer.capacity())
+        buffer.get(bytes)
+        val bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
 
-val bitmap = BitmapFactory.decodeFile(image)
+        var totalRed = 0
+        var totalGreen = 0
+        var totalBlue = 0
+        var pixelCount = 0
 
-val totalRed = 0
-val totalGreen = 0
-val totalBlue = 0
-val pixelCount = 0 //amount of pixels
+        //Get average pixel color
+        for (y in 0..imageHeight) {
+            for (x in 0..imageWidth) {
+                val pixelColor = bitmap.getPixel(x, y)
+                totalRed += Color.red(pixelColor)
+                totalGreen += Color.green(pixelColor)
+                totalBlue += Color.blue(pixelColor)
 
-//Get average pixel color
-for (y in imageHeight) {
-      for (x in imageWidth) {
-          val pixelColor = bitmap.getPixel(x, y)
-          totalRed += Color.red(pixelColor)
-          totalGreen += Color.green(pixelColor)
-          totalBlue += Color.blue(pixelColor)
-          
-          pixelCount += 1
+                pixelCount += 1
+            }
+        }
+        val avgRed = totalRed / pixelCount
+        val avgGreen = totalGreen / pixelCount
+        val avgBlue = totalBlue / pixelCount
+        val avgGray = (avgRed + avgGreen + avgBlue) / 3
+        print("$avgRed, $avgGreen, $avgBlue, $avgGray")
 
-      }
-}
+        //Checking if color is black/white
+        for (y in 0..imageHeight) {
+            for (x in 0..imageWidth) {
+                val pixelColor = bitmap.getPixel(x, y)
+                val redValue = Color.red(pixelColor)
+                val greenValue = Color.green(pixelColor)
+                val blueValue = Color.blue(pixelColor)
+                val grayValue = (redValue + greenValue + blueValue) / 3
 
-val avgRed = totalRed / pixelCount
-val avgGreen = totalGreen / pixelCount
-val avgBlue = totalBlue / pixelCount
-val avgColor = Color.rgb(avgRed, avgGreen, avgBlue)
+                if (grayValue > avgGray) {
+                    imageArray[y][x] = 0 //white
+                } else {
+                    imageArray[y][x] = 1 //black
+                }
+            }
+        }
 
-//Checking if color is black/white
-for (y in imageHeight) {
-      for (x in imageWidth) {
-          val pixelColor = bitmap.getPixel(x, y)
-          val redValue = Color.red(pixelColor)
-          val greenValue = Color.green(pixelColor)
-          val blueValue = Color.blue(pixelColor)
-          
-		  if (redValue > avgRed || greenValue > avgGreen || blueValue > avgBlue) {
-              colorValue = 1 //black
-          } else {
-              colorValue = 0 //white
-          }
-          imageArray[y][x] = colorValue
-
-      }
-}
+        return imageArray
     }
 }
