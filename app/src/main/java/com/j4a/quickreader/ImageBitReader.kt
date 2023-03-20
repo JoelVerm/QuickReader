0b1a10abcf9cbd8af3d452fa18c0ce5b1eceeb55
Joël Vermeulen <98051242+JoelVerm@users.noreply.github.com>
2023-03-20 08:02:02 +0100

    jarmoCode

--- app/src/main/java/com/j4a/quickreader/ImageBitReader.kt
+++ app/src/main/java/com/j4a/quickreader/ImageBitReader.kt
@@ -6,11 +6,81 @@ import android.util.Log
 import androidx.camera.core.ImageProxy
 
 class ImageBitReader(private val image: ImageProxy) {
-    fun read() {
+    fun read(): Array<IntArray> {
         val pixelArray = readColors()
+        return imageToQr(pixelArray)
     }
 
-    fun readColors(): Array<IntArray> {
+    private fun imageToQr(imageArray: Array<IntArray>): Array<IntArray> {
+        val imageHeight = imageArray.size
+        val imageWidth = imageArray[0].size
+        val qrcodeArray = Array(21) { IntArray(21) }
+
+        val storeCounts = mutableListOf<Int>()
+        referenceFound@ for (y in 0 until imageHeight) {
+            var countBlack = 0
+            var countWhite = 0
+            var blackStart = imageArray[0][0]
+            var whiteStart = (imageArray[0][0] * -1 + 1)
+            for (x in 0 until imageWidth) {
+                if (whiteStart == 1) {
+                    if (imageArray[y][x] == 0) {
+                        countWhite += 1
+                    } else {
+                        whiteStart = 0
+                        blackStart = 1
+                        countBlack = 1
+                        storeCounts.add(countWhite)
+                        continue
+                    }
+                }
+                if (blackStart == 1) {
+                    if (imageArray[y][x] == 1) {
+                        countBlack += 1
+                    } else {
+                        whiteStart = 1
+                        blackStart = 0
+                        countWhite = 1
+                        storeCounts.add(countBlack)
+                        continue
+                    }
+                }
+            }
+            if (whiteStart == 1) {
+                storeCounts.add(countWhite)
+            }
+            if (blackStart == 1) {
+                storeCounts.add(countBlack)
+            }
+            if (storeCounts.size == 11) {
+                if (storeCounts[1] == storeCounts[9]) {
+                    var foundPattern = true
+                    val lowerLimit = 6.9
+                    val upperLimit = 7.1
+                    for (num in 2..8) {
+                        if (!(storeCounts[num].toDouble() <= storeCounts[1] / lowerLimit && storeCounts[num].toDouble() >= storeCounts[1] / upperLimit)) {
+                            foundPattern = false
+                        }
+                    }
+                    if (foundPattern) {
+                        val pixelsBox = storeCounts[1] / 7.0
+                        val pixelsMargin = storeCounts[0].toDouble()
+                        for (y_in in 0..20) {
+                            for (x_in in 0..20) {
+                                qrcodeArray[y_in][x_in] =
+                                    imageArray[(pixelsMargin + pixelsBox / 2 + y_in * pixelsBox).toInt()][(pixelsMargin + pixelsBox / 2 + x_in * pixelsBox).toInt()]
+                            }
+                        }
+                        break@referenceFound
+                    }
+                }
+            }
+            storeCounts.clear()
+        }
+        return qrcodeArray
+    }
+
+    private fun readColors(): Array<IntArray> {
         val imageWidth = image.width
         val imageHeight = image.height
         val imageArray = Array(imageHeight) { IntArray(imageWidth) }
