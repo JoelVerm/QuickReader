4b038ac24ea223ba2c4770d87104724a7fe0c4f4
0Jonathan0 <113167625+0Jonathan0@users.noreply.github.com>
2023-03-31 08:44:47 +0200

    copybuttondotcopy

--- app/src/main/java/com/j4a/quickreader/ReaderResult.kt
+++ app/src/main/java/com/j4a/quickreader/ReaderResult.kt
@@ -2,10 +2,13 @@ package com.j4a.quickreader
 
 import android.content.ClipData
 import android.content.ClipboardManager
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class ReaderResult : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,5 +26,18 @@ class ReaderResult : AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
+    }
+    fun setQRResult(value:String) {
+        val textBox = findViewById<TextView>(R.id.resultText)
+        textBox.text = value
+    }
+
+    private fun copyTextToClipboard() {
+        val textBox = findViewById<TextView>(R.id.resultText)
+        val clipboardManager = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        val clipData = ClipData.newPlainText("text", textBox.text)
+        clipboardManager.setPrimaryClip(clipData)
+
+        Toast.makeText(this, "Text copied to clipboard", Toast.LENGTH_LONG).show()
     }
 }
