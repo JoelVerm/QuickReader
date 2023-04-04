f79f272689ea8173fe9041e2e4c01ec0f006b120
0Jonathan0 <113167625+0Jonathan0@users.noreply.github.com>
2023-04-04 21:27:35 +0200

    imagesaver

--- app/src/main/java/com/j4a/quickreader/GenerateResult.kt
+++ app/src/main/java/com/j4a/quickreader/GenerateResult.kt
@@ -1,9 +1,18 @@
 package com.j4a.quickreader
 
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.widget.Button
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStream
 
 class GenerateResult : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,7 +26,28 @@ class GenerateResult : AppCompatActivity() {
 
         val savebutton = findViewById<Button>(R.id.savebutton)
         savebutton.setOnClickListener {
+            saveImageToStorage()
+        }
+    }
 
+    public fun saveImageToStorage(){
+        val externalStorageState = Environment.getExternalStorageState()
+        if (externalStorageState.equals(Environment.MEDIA_MOUNTED)){
+            val storageDirectory = Environment.getExternalStorageDirectory().toString()
+            val file = File(storageDirectory, "test_image.jpg")
+            try {
+                val stream: OutputStream = FileOutputStream(file)
+                bitmap: Bitmap
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream)
+                stream.flush()
+                stream.close()
+                Toast.makeText(this, "image saved succesfully ${Uri.parse(file.absolutePath)}",
+                        Toast.LENGTH_SHORT).show()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        } else {
+            Toast.makeText(this, "Unable to access storage", Toast.LENGTH_SHORT).show()
         }
     }
 }
